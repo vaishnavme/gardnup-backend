b58520147aad44da4ae71855677321967a0976f0
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-28 21:05:02 +0530

    userc route model and controller

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: "Product name is required"
+    },
+    email: {
+        type: String,
+        unique: [true, "email address already in"],
+        match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, "Please Enter valied Email"],
+        required: "email adderess is required"
+    },
+    password: {
+        type: String,
+        minLength: [8, "Password is too short"],
+        required: "password is required"
+    }
+})
+
+// Step Two: Model creation using schema.
+const User = mongoose.model("User", userSchema);
+  
+module.exports = { User };
